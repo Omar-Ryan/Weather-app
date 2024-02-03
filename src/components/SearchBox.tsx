4d9322d0c75859e490c05094a53c5d68df34c8a8
Eng Omar <engomaralryan@gmail.com>
2024-02-03 22:37:21 +0200

    some the addings

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,12 +1,26 @@
+import CN from "@/utils/CN";
 import React from "react";
 import { IoSearch } from "react-icons/io5";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+};
 
 const SearchBox = (props: Props) => {
   return (
-    <form className={"flex relative items-center justify-center h-10"}>
+    <form
+      className={CN(
+        "flex relative items-center justify-center h-10",
+        props.className
+      )}
+      onSubmit={props.onSubmit}
+    >
       <input
+        value={props.value}
+        onChange={props.onChange}
         type="text"
         placeholder="Search location.."
         className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none  focus:border-blue-500 h-full"
